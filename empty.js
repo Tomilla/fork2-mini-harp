70d278b163464f344c250c7cccebc011261a0335
Tomilla <gero.rim@gmail.com>
2014-05-05 23:29:16 +0800

    add: Implement A Current Time Middle-ware
    Implement: The 'app' variable can responds to request path /current-time with
    the current time.
    To_Do: Serving Static Content

--- empty.js
+++ empty.js
@@ -1,6 +1,7 @@
 var connect = require('connect');
-
 var app = connect();
 
 console.log( "starting http servir on http://localhost:4000");
-app.listen( 4000 );
+//connect();
+//app.use(connect.logger());
+app.listen(4000);
--- index.js
+++ index.js
@@ -3,7 +3,18 @@ module.exports = function() {
     var createMiniHarp = require( "connect" )
         , app = createMiniHarp();
 
+    //by default, set port as '4000', avoid port undefined.
+    parsedArgv.port = parsedArgv.port === undefined ? 4000 : parsedArgv.port;
     console.log( "Starting mini-harp on http://localhost:" +
         parsedArgv.port );
+
+    app.use( function( req, res, next) {
+        if  (req.url == "/current-time" ) {
+            res.end( (new Date()).toISOString() );
+        }
+        else {
+            next();
+        }
+    });
     app.listen(parsedArgv.port);
 };
